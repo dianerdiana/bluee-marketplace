5cc701199e8b452eeac7abe7108a9beeaa69aed3
dianerdiana <dianerdiana.de@gmail.com>
2025-10-10 08:28:04 +0700

    Update (component): add card header, title, and body to Card component

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,11 +1,40 @@
 import { cn } from '@/configs/cn';
 
+type TitleTag = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+
 type CardProps = React.HTMLAttributes<HTMLDivElement>;
+type CardHeaderProps = React.HTMLAttributes<HTMLDivElement>;
+type CardBodyProps = React.HTMLAttributes<HTMLDivElement>;
+type CardTitleProps = React.HTMLAttributes<HTMLHeadingElement> & { tag?: TitleTag };
 
 export const Card: React.FC<CardProps> = ({ className, children, ...props }) => {
   return (
-    <div className={cn('bg-white rounded-2xl w-full p-5', className)} {...props}>
+    <div className={cn('card bg-white rounded-2xl w-full', className)} {...props}>
       {children}
     </div>
+  );
+};
+
+export const CardHeader: React.FC<CardHeaderProps> = ({ className, children, ...props }) => {
+  return (
+    <div className={cn('flex flex-wrap justify-between py-5 px-5', className)} {...props}>
+      {children}
+    </div>
+  );
+};
+
+export const CardBody: React.FC<CardBodyProps> = ({ className, children, ...props }) => {
+  return (
+    <div className={cn('pb-5 px-5', className)} {...props}>
+      {children}
+    </div>
+  );
+};
+
+export const CardTitle: React.FC<CardTitleProps> = ({ className, children, tag: Tag = 'h1', ...props }) => {
+  return (
+    <Tag className={`font-bold text-xl text-dark ${className ?? ''}`} {...props}>
+      {children}
+    </Tag>
   );
 };
